1a5ed9ba2470287c9ab5fc55d4e7135174ffb574
macbookair <baghdasaryan.gevorg.97@gmail.com>
2023-04-09 03:11:34 +0400

    add: added mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useState } from "react";
+import "../styles/mobileNavbar.scss";
 import Logotype from "../assets/Logotype.png";
 import Path from "../assets/Path.png";
 import Arrow from "../assets/Arrow.png";
+import Menu from "../assets/Menu.png";
 
 function Navbar() {
+  const [visible, setVisible] = useState("flex")
   window.addEventListener("scroll", () => {
     const scrolled = window.scrollY;
     if (scrolled > 200) {
@@ -14,13 +18,19 @@ function Navbar() {
       element.style.transform = "translateY(0)";
     }
   });
-
+  const showMenu = () => {
+    visible === "flex" ? setVisible("none") : setVisible("flex")
+    console.log(visible)
+}
   return (
     <>
       <div>
         <nav id="menu">
-          <img src={Logotype} />
-          <ul className="container">
+          <span className="heading">
+            <img src={Menu} className="menu" onClick={showMenu} />
+            <img src={Logotype} className="logo" />
+          </span>
+          <ul className="container" style={{display: visible}}>
             <li>
               Demos
               <img src={Path} />
@@ -33,22 +43,19 @@ function Navbar() {
               Features
               <img src={Path} />
               <ul className="submenu">
-                <li className="layout">
+                <li>
                   <span className="grid-container">
                     <span>Post Header</span>
                     <img src={Arrow} />
                   </span>
-                  
-                    <div className="second-submenu">Share Buttons</div>
-                 
                 </li>
-                <li className="">
+                <li className="layout">
                   <span className="grid-container">
                     <span>Post Layout</span>
                     <img src={Arrow} />
                   </span>
+                  <div className="second-submenu">Share Buttons</div>
                 </li>
-
                 <li>
                   <span className="grid-container">
                     <span> Gallery Post</span>
